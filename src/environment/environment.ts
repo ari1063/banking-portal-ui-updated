d4acd1b7dc4798648560fdc412e23079b86af023
ari1063 <47098112+ari1063@users.noreply.github.com>
2025-02-09 21:40:39 +0530

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://banking-portal-api-git-shiftersunited-dev.apps.rm2.thpm.p1.openshiftapps.com/api',
+    apiUrl: 'https://final-api-shiftersunited-dev.apps.rm2.thpm.p1.openshiftapps.com/api',
     tokenName :  "authToken",
     origin:'http://localhost:4200' 
   };
